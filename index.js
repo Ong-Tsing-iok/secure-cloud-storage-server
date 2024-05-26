0213745a95fe018adb380e42e25d46f2f165f5af
Ong-Tsing-iok <86332690+Ong-Tsing-iok@users.noreply.github.com>
2024-05-27 02:21:30 +0800

    Add login authentication procedure

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { Server } from "socket.io";
 import express from "express";
 import winston from "winston";
 import { readFileSync } from "fs";
+import ElGamal from "basic_simple_elgamal";
+import bigInt from "big-integer";
+import { getInRange } from "basic_simple_elgamal/bigIntManager.js";
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +14,9 @@ const options = {
   key: readFileSync("server.key"),
   cert: readFileSync("server.crt"),
 };
+/**
+ * @todo Redirect http to https?
+ */
 const server = createServer(options, app);
 const io = new Server(server, {
   cors: {
@@ -20,7 +26,7 @@ const io = new Server(server, {
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
+  // defaultMeta: { service: "user-service" },
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
@@ -39,22 +45,65 @@ if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
+      level: "debug",
     })
   );
 }
 // server.use(cors())
 
 io.on("connection", (socket) => {
-  logger.log("info", "A client connected");
+  logger.log("info", `Client connected with socket id ${socket.id}`);
 
   socket.on("message", (message) => {
-    logger.log("info", "Received message:", message);
+    logger.log("info", `Received message as server: ${message}`);
     // Broadcast the message to all connected clients
     io.emit("message", message);
   });
 
   socket.on("disconnect", () => {
-    logger.log("info", "A client disconnected");
+    logger.log("info", `Client with socket id ${socket.id} is disconnected`);
+  });
+
+  /**
+   * @todo Generate a random authentication content, encode it with publicKey,
+   * and send back to client for it to decode and send back.
+   */
+  socket.on("login-ask", async (p, g, y) => {
+    logger.info(`Client with socket id ${socket.id} asked to login`);
+    try {
+      socket.elgamal = new ElGamal(p, g, y, "2"); // 2 for private key is filler value (won't use)
+      logger.debug(
+        `p: ${socket.elgamal.modulus}, q: ${socket.elgamal.groupOrder}`
+      );
+      socket.elgamal.checkSecurity(); // TODO: handle errors when p, q, y have problems
+      socket.randKey = await getInRange(bigInt(p).prev(), 1);
+      // socket.randKey = bigInt(56432); // TODO: Should be random generated
+      const cipherPair = await socket.elgamal.encrypt(socket.randKey);
+      socket.emit("login-res", cipherPair.c1, cipherPair.c2);
+    } catch (error) {
+      logger.error(`Error occured when ${socket.id} asked to login: ${error}`);
+      socket.emit("message", "error when login-ask");
+    }
+  });
+  /**
+   * @todo Compare this decode value with the value above.
+   * If is same, then this session is authenticated as this user.
+   * Also check if publickey exist. If not, add to database
+   * (combine register with login)
+   */
+  socket.on("login-auth", (decodeValue) => {
+    decodeValue = bigInt(decodeValue);
+    if (socket.randKey.compare(decodeValue) == 0) {
+      logger.info(`Client with socket id ${socket.id} is authenticated`);
+      socket.authed = true;
+      socket.emit("login-auth-res", "OK");
+    } else {
+      logger.info(
+        `Client with socket id ${socket.id} respond with incorrect authentication`
+      );
+      logger.debug(`respond with ${decodeValue} instead of ${socket.randKey}`);
+      socket.emit("login-auth-res", "incorrect");
+    }
   });
 });
 
